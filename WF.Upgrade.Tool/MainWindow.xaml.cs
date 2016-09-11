7336b368f94438da98913a09e925ec988216dec8
kunta0514 <kunta0514@163.com>
2016-09-11 19:26:57 +0800

    '修复cef问题，补充框架页面'

--- WF.Upgrade.Tool/MainWindow.xaml.cs
+++ WF.Upgrade.Tool/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WFUpdatetool
+namespace WF.Upgrade.Tool
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
@@ -23,16 +25,42 @@ namespace WFUpdatetool
         public MainWindow()
         {
             InitializeComponent();
+            init();
         }
 
         private void init()
         {
-            var address = Environment.CurrentDirectory + @"\html\site\index.html?Rand=" + DateTime.Now;
+            var Address = Environment.CurrentDirectory + @"\view\html\site\index.html?Rand=" + DateTime.Now;
+            //var Address = "http://10.5.106.25:1111/";
+            //cef.Address = Environment.CurrentDirectory + @"\view\html\site\index.html?Rand=" + DateTime.Now;
 
-            cwb.Address = address;
+            cef.Address = Address;
+
+            cef.RegisterJsObject("callbackObj", new CallbackObjectForJs());
+            //cef.PreviewTextInput += (o, e) =>
+            //{
+            //    foreach (var character in e.Text)
+            //    {
+            //        // 把每个字符向浏览器组件发送一遍
+            //        cef.GetBrowser().GetHost().SendKeyEvent((int)WM.CHAR, (int)character, 0);
+            //    }
+
+            //    // 不让cef自己处理
+            //    e.Handled = true;
+            //};
+
+            //cwb.Address = address;
 
             //cwb.RegisterJsObject("SiteInfoData", new SiteService());
             //cwb.
+        }
+
+        public class CallbackObjectForJs
+        {
+            public void showMessage(string msg)
+            {
+                MessageBox.Show(msg);
+            }
         }
     }
 }
